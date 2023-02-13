2af9e853a833e80e369542a3cfe5e5ff8bf6dcd4
Ivan Kelava <ivan.kelava236@gmail.com>
2023-02-13 18:54:43 +0100

    commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Hello world!");
+
+const date = document.querySelector('#date')
+
+date.addEventListener('click', function(){
+    let date1 = new Date();
+    date.innerHTML = date1.toLocaleString()
+
+});
